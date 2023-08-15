ffeefdc9a2458210ca491b4725178475a1eb6071
HassanBeiruty <hassanalbeiruty@gmail.com>
2023-08-15 15:07:24 +0300

    first commit

---
+++ src/main/java/quiz_pages/DesignPatterns/Adapter/JsonUserAdapter.java
@@ -0,0 +1,41 @@
+package quiz_pages.DesignPatterns.Adapter;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import quiz_pages.Models.User;
+
+public class JsonUserAdapter {
+
+    private final File jsonFile;
+    private final ObjectMapper objectMapper;
+
+    public JsonUserAdapter() {
+        this.jsonFile = new File("json/currentUser.json");
+        this.objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+    }
+
+    public User getUser() {
+        User user = new User();
+        try {
+            if (jsonFile.exists()) {
+                user = objectMapper.readValue(jsonFile, User.class);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+
+    public void changeUser(User item) {
+        User user = new User(item.getName(),item.getEmail(), item.getPassword(), item.getGender(), item.getDate(), item.getScore());
+        try {
+            objectMapper.writeValue(jsonFile, user);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
